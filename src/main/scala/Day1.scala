2bb7ccf5997dd6f4b5a8066aa1a4eb51a68218da
Giulio Mazzanti <gmazzanti@protonmail.com>
2020-12-01 06:45:51 +0100

    chore: improve style and solution code in separate function

--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -3,14 +3,27 @@ import scala.io.Source
 object Day1 {
   def main(args: Array[String]) {
     val path: String = {
-      if (args.length == 0) 
+      if (args.length == 0)
         "./input.txt"
       else
         args(0)
     }
 
     val list: List[Int] = Source.fromFile(path).getLines.toList.map(_.toInt)
-    println("First answer: " + list.combinations(2).toList.view.filter{ case List(a, b) => a + b == 2020 }.head.product)
-    println("Second answer: " + list.combinations(3).toList.view.filter{ case List(a, b, c) => a + b + c == 2020 }.head.product)
+    def solve(l: List[Int], nCombinations: Int): Int = {
+      l.combinations(nCombinations)
+        .toList
+        .view
+        .filter { case c: List[Int] => c.sum == 2020 }
+        .head
+        .product
+    }
+
+    println(
+      "First answer: " + solve(list, 2)
+    )
+    println(
+      "Second answer: " + solve(list, 3)
+    )
   }
 }
