b351323558a95a6437e306d459b80cef1986f67a
Giulio Mazzanti <gmazzanti@protonmail.com>
2020-12-03 07:04:16 +0100

    chore: add Slope case class

--- src/main/scala/Day3.scala
+++ src/main/scala/Day3.scala
@@ -10,34 +10,35 @@ object Day3 {
         args(0)
     }
 
-    def countTraverse[A](toFind: A)(matrix: Vector[Vector[A]], right: Int, down: Int): Int = {
+    case class Slope(right: Int, down: Int)
+    def countTraverse[A](toFind: A)(matrix: Vector[Vector[A]], slope: Slope): Int = {
       @tailrec def accTraverse(row: Int, column: Int, count: Int): Int = {
         if (row >= matrix.length)
           count
         else {
           val currentRow = matrix(row)
           val newCount = if (currentRow(column % currentRow.length) == toFind) count + 1 else count
-          accTraverse(row + down, column + right, newCount)
+          accTraverse(row + slope.down, column + slope.right, newCount)
         }
       }
       accTraverse(0, 0, 0)
     }
 
     val mat: Vector[Vector[Char]] = Source.fromFile(path).getLines.toVector.map(_.toVector)
-    def solve = countTraverse[Char]('#')(_, _, _)
+    def solve = countTraverse[Char]('#')(_, _)
 
     println(
-      "First answer: " + solve(mat, 3, 1)
+      "First answer: " + solve(mat, Slope(3, 1))
     )
 
-    val possibleSlopes = List(
-      (1, 1),
-      (3, 1),
-      (5, 1),
-      (7, 1),
-      (1, 2)
+    val slopes = List(
+      Slope(1, 1),
+      Slope(3, 1),
+      Slope(5, 1),
+      Slope(7, 1),
+      Slope(1, 2)
     )
-    val counts: List[BigInt] = possibleSlopes.map(slope => solve(mat, slope._1, slope._2))
+    val counts: List[BigInt] = slopes.map(solve(mat, _))
 
     println(
       "Second answer: " + counts.product
