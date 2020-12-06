d22ba49ba1d0383204eec963f7917be248b9e2f7
Giulio Mazzanti <gmazzanti@protonmail.com>
2020-12-06 06:23:57 +0100

    feat: add Day6 solution

---
+++ src/main/scala/Day6.scala
@@ -0,0 +1,22 @@
+import scala.io.Source
+
+object Day6 {
+  def main(args: Array[String]) {
+    val path: String = {
+      if (args.length == 0)
+        "./input.txt"
+      else
+        args(0)
+    }
+
+    val groups: List[String] =
+      Source.fromFile(path).getLines.mkString("\n").split("\n\n").toList
+
+    println(
+      "First answer: " + groups.map(_.replace("\n", "").toSet.size).sum
+    )
+    println(
+      "Second answer: " + groups.map(_.split("\n").map(_.toSet).reduce(_.intersect(_)).size).sum
+    )
+  }
+}
