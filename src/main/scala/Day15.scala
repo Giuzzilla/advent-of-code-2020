0d6afe63c3a4e823f15059bc56b27749512e5e25
Giulio Mazzanti <gmazzanti@protonmail.com>
2020-12-16 09:33:09 +0100

    perf(15): switch from BigInt to Long in recursive solution for saving memory

--- src/main/scala/Day15.scala
+++ src/main/scala/Day15.scala
@@ -12,29 +12,29 @@ object Day15 {
 
     val list = input.split(",").map(_.toInt).toList
 
-    def solution(lst: List[Int], steps: BigInt): BigInt = {
+    def solution(lst: List[Int], steps: Long): Long = {
       @tailrec
       def recFunc(
-          memory: Map[BigInt, BigInt],
-          last: BigInt,
-          idx: BigInt
-      ): BigInt = {
+          memory: Map[Int, Long],
+          last: Int,
+          idx: Long
+      ): Long = {
         if (idx == steps - 1)
           last
         else {
           val nextVal =
             if (memory.contains(last))
-              idx - memory(last)
+              (idx - memory(last)).toInt
             else
-              BigInt(0)
+              0
 
           recFunc(memory + (last -> idx), nextVal, idx + 1)
         }
       }
       recFunc(
-        lst.zipWithIndex.map(t => (BigInt(t._1), BigInt(t._2))).toMap,
-        BigInt(lst.last),
-        BigInt(lst.length - 1)
+        lst.zipWithIndex.map(t => (t._1, t._2.toLong)).toMap,
+        lst.last,
+        (lst.length - 1).toLong
       )
     }
 
