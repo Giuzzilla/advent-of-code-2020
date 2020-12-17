bb8bb4c143fae25453b178d3f67692788996c08a
Giulio Mazzanti <gmazzanti@protonmail.com>
2020-12-17 07:19:56 +0100

    style(17): format code

--- src/main/scala/Day17.scala
+++ src/main/scala/Day17.scala
@@ -1,7 +1,6 @@
 import scala.io.Source
 import scala.annotation.tailrec
 
-
 case class Cube(x: Int, y: Int, z: Int, w: Int = 0, active: Boolean = false) {
   def areClose(a: Int, b: Int): Boolean = {
     val diff = a - b
@@ -9,9 +8,9 @@ case class Cube(x: Int, y: Int, z: Int, w: Int = 0, active: Boolean = false) {
   }
 
   def neighbors(others: List[Cube]) = {
-    others.filter {
-      c =>
-      areClose(c.x, x) && areClose(c.y, y) && areClose(c.z, z) && areClose(c.w, w) &&
+    others.filter { c =>
+      areClose(c.x, x) && areClose(c.y, y) &&
+      areClose(c.z, z) && areClose(c.w, w) &&
       (c.x != x || c.y != y || c.z != z || c.w != w)
     }
   }
@@ -19,7 +18,7 @@ case class Cube(x: Int, y: Int, z: Int, w: Int = 0, active: Boolean = false) {
   def evolve(others: List[Cube]): Cube = {
     val numActive = neighbors(others).filter(_.active).length
     val newState = if (active) {
-    numActive == 2 || numActive == 3
+      numActive == 2 || numActive == 3
     } else {
       numActive == 3
     }
@@ -29,10 +28,12 @@ case class Cube(x: Int, y: Int, z: Int, w: Int = 0, active: Boolean = false) {
   def canEqual(a: Any) = a.isInstanceOf[Cube]
 
   override def equals(other: Any): Boolean = other match {
-    case other: Cube => other.canEqual(this) && x == other.x && y == other.y && z == other.z && w == other.w
+    case other: Cube =>
+      other.canEqual(this) &&
+      x == other.x && y == other.y &&
+      z == other.z && w == other.w
     case _ => false
   }
-
 }
 
 object Day17 {
@@ -51,7 +52,12 @@ object Day17 {
       active = if (el == '#') true else false
     } yield Cube(rowId, colId, 0, 0, active)
 
-    def doCycles(cubeList: List[Cube], secondStar: Boolean = false, cycle: Int = 0, maxCycle: Int = 6): List[Cube] = {
+    def doCycles(
+        cubeList: List[Cube],
+        secondStar: Boolean = false,
+        cycle: Int = 0,
+        maxCycle: Int = 6
+    ): List[Cube] = {
       if (cycle == maxCycle)
         cubeList
       else {
@@ -59,7 +65,7 @@ object Day17 {
         val ys = cubeList.map(_.y).distinct
         val zs = cubeList.map(_.z).distinct
         val ws = cubeList.map(_.w).distinct
-        val wsRange = if(secondStar) (ws.min - 1 to ws.max + 1) else (0 to 0)
+        val wsRange = if (secondStar) (ws.min - 1 to ws.max + 1) else (0 to 0)
         val newCubeList = (for {
           x <- xs.min - 1 to xs.max + 1
           y <- ys.min - 1 to ys.max + 1
@@ -67,16 +73,16 @@ object Day17 {
           w <- wsRange
           newCube = Cube(x, y, z, w, false)
           if (!cubeList.contains(newCube))
-        } yield(newCube)).toList ::: cubeList
+        } yield (newCube)).toList ::: cubeList
         val evolvedCubes = newCubeList.map(_.evolve(cubeList)).filter(_.active)
 
         doCycles(evolvedCubes, secondStar, cycle + 1, maxCycle)
       }
     }
-   
+
     println(
-        "First answer: " + doCycles(cubes).length
-      )
+      "First answer: " + doCycles(cubes).length
+    )
     println(
       "Second answer: " + doCycles(cubes, true).length
     )
