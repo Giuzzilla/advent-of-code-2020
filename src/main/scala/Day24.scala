d1f528ae3eeda333b6709912e8a423f8cb7e0158
Giulio Mazzanti <gmazzanti@protonmail.com>
2020-12-24 10:56:03 +0100

    style(24): formatting

--- src/main/scala/Day24.scala
+++ src/main/scala/Day24.scala
@@ -1,7 +1,6 @@
 import scala.annotation.tailrec
 import scala.io.Source
 
-
 object Day24 {
   def main(args: Array[String]) {
     val path: String = {
@@ -14,10 +13,10 @@ object Day24 {
     type Tile = (Int, Int)
 
     val dirMap = Map(
-      "sw"-> (-1, -1),
-      "se"-> (-1, 1),
-      "nw"-> (1, -1),
-      "ne"-> (1, 1),
+      "sw" -> (-1, -1),
+      "se" -> (-1, 1),
+      "nw" -> (1, -1),
+      "ne" -> (1, 1),
       "e" -> (0, 2),
       "w" -> (0, -2)
     )
@@ -28,16 +27,20 @@ object Day24 {
         matched <- patt.findAllMatchIn(s)
         pos = dirMap(matched.group(1))
       } yield pos
-      val finalPos = stepsLst.foldLeft((0,0))(
-        (acc, step) => 
-          (acc._1 + step._1, acc._2 + step._2)
+      val finalPos = stepsLst.foldLeft((0, 0))((acc, step) =>
+        (acc._1 + step._1, acc._2 + step._2)
       )
       finalPos
     }
 
     val list = Source.fromFile(path).getLines.toList.map(parseTile(_))
 
-    val initBlacks = list.groupBy(identity).map(t => t._1->t._2.size).filter(_._2 % 2 != 0).keys.toSet
+    val initBlacks = list
+      .groupBy(identity)
+      .map(t => t._1 -> t._2.size)
+      .filter(_._2 % 2 != 0)
+      .keys
+      .toSet
 
     def generateAdjacent(tile: Tile): List[Tile] = {
       val steps = dirMap.values.toList
@@ -45,7 +48,11 @@ object Day24 {
     }
 
     @tailrec
-    def solve2(blacks: Set[Tile], maxIters: Int = 100, iter: Int = 0): Set[Tile] = {
+    def solve2(
+        blacks: Set[Tile],
+        maxIters: Int = 100,
+        iter: Int = 0
+    ): Set[Tile] = {
       if (iter == maxIters)
         blacks
       else {
@@ -56,17 +63,19 @@ object Day24 {
         val newBlacks = (for {
           tile <- allTiles
           adjs = generateAdjacent(tile).filter(blacks.contains(_))
-        } yield (tile, adjs.length)).filter{
-          case (k, v) =>
+        } yield (tile, adjs.length))
+          .filter { case (k, v) =>
             if (blacks.contains(k))
               !(v == 0 || v > 2)
             else
               v == 2
-        }.toList.map(_._1).toSet
+          }
+          .toList
+          .map(_._1)
+          .toSet
         solve2(newBlacks, maxIters, iter + 1)
       }
     }
-
 
     println(
       "First answer: " + initBlacks.size
