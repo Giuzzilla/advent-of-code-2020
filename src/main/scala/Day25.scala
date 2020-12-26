256d1e5e1b33b1bc89aa1fcbf77ab415b03d6bb9
Giulio Mazzanti <gmazzanti@protonmail.com>
2020-12-26 16:48:46 +0100

    style(25): minor formatting

--- src/main/scala/Day25.scala
+++ src/main/scala/Day25.scala
@@ -1,7 +1,6 @@
 import scala.annotation.tailrec
 import scala.io.Source
 
-
 object Day25 {
   def main(args: Array[String]) {
     val path: String = {
@@ -13,14 +12,19 @@ object Day25 {
 
     val pks = Source.fromFile(path).getLines.toList.map(_.toInt)
 
-    def calc(maxLoop: Int, subj: Int,  key: Int, accValue: BigInt = BigInt(1), iter: Int = 0): (Int, BigInt) = {
+    def calc(
+        maxLoop: Int,
+        subj: Int,
+        key: Int,
+        accValue: BigInt = BigInt(1),
+        iter: Int = 0
+    ): (Int, BigInt) = {
       if (accValue == key || iter == maxLoop)
         (iter, accValue)
       else {
         calc(maxLoop, subj, key, (accValue * subj) % 20201227, iter + 1)
       }
     }
-
 
     val (loop1, _) = calc(Int.MaxValue, 7, pks(0))
     val (loop2, _) = calc(Int.MaxValue, 7, pks(1))
